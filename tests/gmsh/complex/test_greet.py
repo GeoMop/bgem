3e3ef1aec876fab9acfdcb4349fc6337681b5a2a
Paulie14 <pavelexner@gmail.com>
2020-03-26 17:14:50 +0100

    Fix Greet tunnel fuse test, add similar one.

--- tests/gmsh/complex/test_greet.py
+++ tests/gmsh/complex/test_greet.py
@@ -25,7 +25,8 @@ def geometry():
             name="tunnel_2",
             radius=2.5,
             start=[0, 17.180586073, -3],
-            end=[0, -29.319723669, -3]
+            # end=[0, -29.319723669, -3] -> increase the angle fort better visualization
+            end=[0, -29.319723669, -50]
         )
     )
 
@@ -183,14 +184,13 @@ def test_greet_no_volume():
     # gen.show()
 
 
-
 def test_fuse_tunnel():
     """
     Test shows, how to fuse two cylinders cross-secting each in a common plane under given angle.
     It prolongates the cylinders, creates the common face in the middle, cuts the original cylinders
     and then fuses them into final object.
     """
-    mesh_name = "tunnel"
+    mesh_name = "fuse_tunnel"
     gen = gmsh.GeometryOCC(mesh_name, verbose=True)
     geom = geometry()
 
@@ -199,8 +199,8 @@ def test_fuse_tunnel():
     tunnel_mid = np.array(geom['tunnel_1']["end"])
     tunnel_end = np.array(geom['tunnel_2']["end"])
 
-    tunnel_1_c = create_cylinder(gen, geom['tunnel_1'], 0.2)
-    tunnel_2_c = create_cylinder(gen, geom['tunnel_2'], 0.2)
+    tunnel_1_c = create_cylinder(gen, geom['tunnel_1'], 0.2)[0]
+    tunnel_2_c = create_cylinder(gen, geom['tunnel_2'], 0.2)[0]
 
     # cutting box
     box_s = 200
@@ -254,6 +254,107 @@ def test_fuse_tunnel():
     gen.make_mesh(mesh_all)
     print("Generating mesh...[finished]")
     print("Writing mesh...")
-    gen.write_mesh(mesh_name, gmsh.MeshFormat.msh2)
+    gen.write_mesh(mesh_name + ".msh2", gmsh.MeshFormat.msh2)
+    print("Writing mesh...[finished]")
+    # gen.show()
+
+
+def test_fuse_tunnel_2():
+    """
+    Test shows, how to fuse two cylinders cross-secting each in a common plane under given angle.
+    In contrast to previous test, we rotate the second tunnel around extremal point EP of the intersetion ellipse
+    and then fuses both into final object.
+
+    -> RESULT: not a good approach - the point EP is found correctly, however the cylinders stick out
+    which can be seen both in brep and even better in resulting mesh with finer mesh step.
+    """
+    mesh_name = "fuse_tunnel_2"
+    gen = gmsh.GeometryOCC(mesh_name, verbose=True)
+    geom = geometry()
+
+    # create tunnel
+    tunnel_start = np.array(geom['tunnel_1']["start"])
+    tunnel_mid = np.array(geom['tunnel_1']["end"])
+    tunnel_end = np.array(geom['tunnel_2']["end"])
+
+    radius = float(geom['tunnel_1']["radius"])
+    assert radius == float(geom['tunnel_2']["radius"])
+
+    # # cutting box
+    box_s = 20
+    # box = gen.box([box_s, box_s, box_s])
+    # # cutting plane between the cylinders
+    plane = gen.rectangle([box_s, box_s]).rotate([1, 0, 0], np.pi / 2)
+
+    # directional vectors of the cylinders
+    # supposing equal radius
+    v_t1 = tunnel_mid - tunnel_start
+    v_t1 = v_t1 / np.linalg.norm(v_t1)  # normalize
+    v_t2 = tunnel_mid - tunnel_end
+    v_t2 = v_t2 / np.linalg.norm(v_t2)  # normalize
+
+    # directional vectors of the cutting plane
+    angle_12 = np.pi - np.arccos(np.dot(v_t1, v_t2))
+    d = radius / np.sin(angle_12)
+    u = d*(v_t1 + v_t2) # points from the center to the extremal point of the ellipse E
+
+    # distance we have to move both cylinder, so they meet at E with their corners
+    delta = np.sqrt(np.dot(u, u) - radius*radius)
+
+    # create prolongated tunnels for fusion
+    tunnel_mid_1 = tunnel_mid + delta * v_t1
+    tunnel_mid_2 = tunnel_mid + delta * v_t2
+    tunnel_1 = gen.cylinder(radius, tunnel_mid_1 - tunnel_start, tunnel_start)
+    tunnel_2 = gen.cylinder(radius, tunnel_mid_2 - tunnel_end, tunnel_end)
+
+
+    # if needed, make the cutting ellipse plane
+    v = np.cross(v_t1, v_t2)
+    # normal of the cutting plane
+    n = np.cross(u, v)
+    n = n / np.linalg.norm(n)  # normalize
+
+    # angle between cutting plane and y-axis
+    angle = np.arccos(np.dot([0, 0, 1], n)) - np.pi / 2
+    # rotate cutting box and plane
+    plane.rotate(axis=[1, 0, 0], angle=angle)
+
+    # move the cutting plane into the connecting point
+    plane.translate(tunnel_mid)
+
+
+    print("fuse...")
+    # tunnel = tunnel_1.copy().fuse(tunnel_2.copy())
+    tunnel = tunnel_1.fuse(tunnel_2)
+    # tunnel = tunnel_2_x.fuse(tunnel_1_x)
+    tunnel.set_region("tunnel")
+    mesh_step = 1.0
+    tunnel.set_mesh_step(mesh_step)
+
+    # tunnel_3 = gen.cylinder(radius, tunnel_mid - tunnel_start, tunnel_start)
+    # tunnel_4 = gen.cylinder(radius, tunnel_mid - tunnel_end, tunnel_end)
+    # tunnel_1.set_region("tunnel_1")
+    # tunnel_1.set_mesh_step(mesh_step)
+    # tunnel_2.set_region("tunnel_2")
+    # tunnel_2.set_mesh_step(mesh_step)
+    # tunnel_3.set_region("tunnel_3")
+    # tunnel_3.set_mesh_step(mesh_step)
+    # tunnel_4.set_region("tunnel_4")
+    # tunnel_4.set_mesh_step(mesh_step)
+
+    # mesh_all = [tunnel]
+
+    # uncomment one of the following (and the corresponding above) to see auxilliary objects
+    mesh_all = [tunnel, plane]
+    # mesh_all = [plane, tunnel_1, tunnel_2]
+    # mesh_all = [tunnel_1, tunnel_2, tunnel, plane, tunnel_3, tunnel_4]
+
+    print("Generating mesh...")
+    gen.keep_only(*mesh_all)
+    gen.write_brep()
+    gen.make_mesh(mesh_all)
+    print("Generating mesh...[finished]")
+    print("Writing mesh...")
+    gen.write_mesh(mesh_name + ".msh2", gmsh.MeshFormat.msh2)
     print("Writing mesh...[finished]")
     # gen.show()
