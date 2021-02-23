87669bc42bacac43d7e89a4193e94282029dec50
Pavel Exner <pavelexner@gmail.com>
2021-02-23 15:19:57 +0100

    Add line in gmsh api.

--- src/bgem/gmsh/gmsh.py
+++ src/bgem/gmsh/gmsh.py
@@ -287,6 +287,16 @@ class GeometryOCC:
         self._need_synchronize = True
         return self.object(dim, res)
 
+    def line(self, a, b):
+        """
+        Make line between points a,b.
+        return: Object set with a single dimtag.
+        """
+        point_ids = [self.model.addPoint(*p) for p in [a,b]]
+        res = self.model.addLine(*point_ids)
+        self._need_synchronize = True
+        return self.object(1, res)
+
     def rectangle(self, xy_sides=[1, 1], center=[0, 0, 0]):
         """
         TODO: Better match GMSH API, possibly use origin as the default left corner.
