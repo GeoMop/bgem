b1f4d2ac9e49de3e2a6b6690bd5d41db4e20b5d2
Jan Brezina <jan.brezina@tul.cz>
2022-11-21 21:57:42 +0100

    fix the fix

--- src/bgem/bspline/bspline_plot.py
+++ src/bgem/bspline/bspline_plot.py
@@ -89,7 +89,7 @@ class PlottingMatplot:
         self.fig_2d = plt.figure(1)
         self.fig_3d = plt.figure(2)
         #self.ax_3d = self.fig_3d.gca(projection='3d')
-        self.ax_3d = self.add_subplot(projection='3d')
+        self.ax_3d = self.fig_3d.add_subplot(projection='3d')
 
     def add_curve_2d(self, X, Y, **kwargs):
         plt.figure(1)
